320c46be99d00cc0fe055e5502faf5020326a28e
mharquail <mharquail95@gmail.com>
2021-03-25 17:26:41 -0400

    Initial Commit of game files
    - Created basic desktop environment
    - Added pause menu functionality
    - Create CLI scene

---
+++ Assets/CLIScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CLIScript : MonoBehaviour{
+	public string consoleDisplay;
+	public GameObject inputField;
+	public GameObject textDisplay;
+
+	public void showText(){
+		consoleDisplay = inputField.GetComponent<Text>().text;
+		textDisplay.GetComponent<Text>().text = ">"+consoleDisplay;
+	}
+}
