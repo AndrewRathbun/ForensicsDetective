8d9c73bac5c8b659b78b7509212880ab558ec4cc
Kevin Baumgartner <43413002+kevjbaumgartner@users.noreply.github.com>
2021-07-10 22:09:21 -0400

    GameFile & GenerateFile Scripts
    Created 2 scripts to handle in-game "GameFile" class objects, as well the creation and storage.

---
+++ Assets/Scripts/GameFile.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameFile : MonoBehaviour
+{
+
+    //Variable declarations
+    private string fname, fextension, fsize, fpath, fdate, fcontent;
+
+    //File object constructors
+    public GameFile(string fileName, string fileExtension, string fileSize, string filePath, string fileDate, string fileContent){
+        fname = fileName;
+        fextension = fileExtension;
+        fsize = fileSize;
+        fpath = filePath;
+        fdate = fileDate;
+        fcontent = fileContent;
+    }
+
+    //Getters
+    public string getGameFileName(){
+        return fname;
+    }
+
+    public string getGameFileExtension(){
+        return fextension;
+    }
+
+    public string getGameFileSize(){
+        return fsize;
+    }
+
+    public string getGameFilePath(){
+        return fpath;
+    }
+
+    public string getGameFileDate(){
+        return fdate;
+    }
+
+    public string getGameFileContent(){
+        return fcontent;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/GenerateFile.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateFile : MonoBehaviour
+{
+
+    List<GameFile> files = new List<GameFile>();
+
+    List<string> nameArray = new List<string>{"notAVirus", "Valorant", "Gooqle"};
+    List<string> extensionArray = new List<string>{".exe", ".mp4", ".bin"};
+    List<string> sizeArray = new List<string>{"100 KB", "200 MB", "3 GB"};
+    List<string> pathArray = new List<string>{"C:/", "D:/", "E:/"};
+    List<string> dateArray = new List<string>{"7/10/2021", "4/15/2021", "31/12/1999"};
+    List<string> contentArray = new List<string>{"HelloWorld", "execute(launchorder666)", "Bacon Cheeseburger"};
+
+    public GameFile createFile(){
+        GameFile temp = new GameFile(
+            nameArray[Random.Range(0, nameArray.Count - 1)],
+            extensionArray[Random.Range(0, extensionArray.Count - 1)],
+            sizeArray[Random.Range(0, sizeArray.Count - 1)],
+            pathArray[Random.Range(0, pathArray.Count - 1)],
+            dateArray[Random.Range(0, dateArray.Count - 1)],
+            contentArray[Random.Range(0, contentArray.Count - 1)]
+        );
+
+        return temp;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            files.Add(createFile());
+        }
+
+        for (int i = 0; i < files.Count; i++)
+        {
+            Debug.Log(files[i].getGameFileName());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
